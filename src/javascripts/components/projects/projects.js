70651d242e38926234d009e1a794deded235b5ef
Heath JH Moore <heathjhmoore@gmail.com>
2019-06-22 13:43:23 -0500

    setup stuff

---
+++ src/javascripts/components/projects/projects.js
@@ -0,0 +1,31 @@
+import getProjects from './getProjects';
+import util from '../../helpers/util';
+
+const projectBuilder = () => {
+  getProjects.getProjects()
+    .then((projects) => {
+      console.error(projects);
+      let domString = '';
+      projects.forEach((project) => {
+        domString += '<div class="col-12 col-sm-12 col-md-6 col-lg-4">';
+        domString += `<div class="card" id="${project.id}">`;
+        domString += '<div class="card-body">';
+        domString += `<h5 class="card-title">${project.name}</h5>`;
+        domString += '<p class="card-text">Another cool project for my portfolio</p>';
+        domString += '<h4 class="text-center" style="border-bottom: solid 1px gray;">Authors</h4>';
+        domString += '<div class="row d-flex justify-content-around">';
+        project.authors.forEach((author) => {
+          domString += `<p class="col-3 col-md-4 text-center">${author}</p>`;
+        });
+        domString += '</div>';
+        domString += `<a href=${project.url} class="btn btn-primary" target="_blank">Check out this project!</a>`;
+        domString += '</div>';
+        domString += '</div>';
+        domString += '</div>';
+      });
+      util.printToDom('projectsContainer', domString);
+    })
+    .catch();
+};
+
+export default { projectBuilder };
---
+++ src/javascripts/main.js
@@ -0,0 +1,9 @@
+import 'bootstrap';
+import '../styles/main.scss';
+import projects from './components/projects/projects';
+
+const init = () => {
+  projects.projectBuilder();
+};
+
+init();
