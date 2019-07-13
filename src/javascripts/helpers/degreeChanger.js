8f1a426a1d4da893a6c67610a5393d903b772a8b
Heath JH Moore <heathjhmoore@gmail.com>
2019-07-13 00:23:45 -0500

    done styling

--- src/javascripts/helpers/degreeChanger.js
+++ src/javascripts/helpers/degreeChanger.js
@@ -2,9 +2,8 @@ let degree = 0
 
 const degreeChange = () => {
     degree += 1;
-    console.error(degree)
     // const backgroundGradient = `linear-gradient(${degree}deg, white, rgba(200, 200, 200, .4), white, rgba(200, 200, 200, .4), white)`;
-    const backgroundGradient = `linear-gradient(${degree}deg, rgba(200, 200, 200, .4), white, rgba(200, 200, 200, .4), white, rgba(200, 200, 200, .4))`;
+    const backgroundGradient = `linear-gradient(${degree}deg, rgba(200, 200, 200, .4), white,  white, rgba(200, 200, 200, .4))`;
     // const backgroundGradient = `linear-gradient(${degree}deg, rgb(200,82,111), rgb(80,175,207))`;
     document.getElementById('technologiesContainer').style.background = backgroundGradient
 };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -3,8 +3,12 @@ import '../styles/main.scss';
 import projects from './components/projects/projects';
 import scroller from './helpers/scrollPosition';
 import degree from './helpers/degreeChanger';
+import quote from './components/Quotes/Quotes';
 
 const init = () => {
+  setInterval(() => {
+    quote.quoteChanger();
+  }, 5000);
   projects.projectBuilder();
   scroller.scrollListener();
   setInterval(() => {
